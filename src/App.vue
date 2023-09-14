40ca3567b97c4e6d652b27ab97021d34803146cb
thanyalukkk <thanyaluksukkasem@gmail.com>
2023-09-15 01:47:59 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,11 @@ import { ref } from 'vue';
 
 const travellist = ref ([
   {name:'ท่าช้าง คาเฟ่', style:'ร้านอาหารกึ่งผับ กึ่งบาร์', service:'อาหาร เครื่องดื่ม ดนตรี',time:'17:00-23:59', img:"https://p-u.popcdn.net/event_details/posters/000/008/856/original/cfe1b94d03c5137faf1500f98117421e9f2ac9db.jpg?1590662796"},
-  {name:'THE GOOD VIEW BAR & RESTAURANT', style:'ร้านอาหารกึ่งบาร์', service:'อาหาร เครื่องดื่ม ดนตรี คาราโอเกะ',time:'10:00-01:00', img:"http://www.goodview.co.th/2019/images/goodview/sm-goodview-pic15.jpg"},
+  {name:'THE GOOD VIEW BAR & RESTAURANT', style:'ร้านอาหารกึ่งบาร์', service:'อาหาร เครื่องดื่ม ดนตรี คาราโอเกะ',time:'10:00-01:00', img:"https://www.awaygpub.com/wp-content/uploads/2021/03/The-Good-View-Bar-and-Restaurant-%E0%B8%9A%E0%B8%B2%E0%B8%A3%E0%B9%8C%E0%B8%A7%E0%B8%B4%E0%B8%A7%E0%B8%94%E0%B8%B5%E0%B8%97%E0%B8%B5%E0%B9%88%E0%B8%AA%E0%B8%B8%E0%B8%94%E0%B9%83%E0%B8%99%E0%B9%80%E0%B8%8A%E0%B8%B5%E0%B8%A2%E0%B8%87%E0%B9%83%E0%B8%AB%E0%B8%A1%E0%B9%88awaygpub-2.jpg"},
   {name:'The Riverside Bar & Restaurant' , style:'ร้านอาหาร กึ่งบาร์', service:'อาหาร เครื่องดื่ม ดนตรี',time:'10:00-01:00', img:"https://th.bing.com/th/id/R.ab9802205edc889cdc62f84af245316c?rik=EUFmNFFfO91eJA&riu=http%3a%2f%2fwww.dooasia.com%2fwp-content%2fuploads%2f2018%2f05%2fThe-RiverSide-%e0%b9%80%e0%b8%8a%e0%b8%b5%e0%b8%a2%e0%b8%87%e0%b9%83%e0%b8%ab%e0%b8%a1%e0%b9%88-7-696x464.jpg&ehk=IfTrEAF8BEuE9hJhtMKHQNYZmV%2boSJEesY%2fzQmFNYoo%3d&risl=&pid=ImgRaw&r=0"},
   {name:'WarmUp Cafe', style:'ร้านอาหารนั่งชิลล์', service:'อาหาร เครื่องดื่ม ดนตรี',time:'18:00-01:00', img:"https://d136usn7jnoe61.cloudfront.net/pictures/21526/s2n_0002_p1d06tc4oou8jlkvr0l1mag1grj6.jpg"},
   {name:'ตะวันแดง มหาชน', style:'ผับ', service:'อาหาร เครื่องดื่ม ดนตรี',time:'19:00-02:00', img:"https://th.bing.com/th/id/R.6fd3de7109e6a325c0d72efb53de2050?rik=Aa1zQxZXAWtxSg&riu=http%3a%2f%2ftawandang.com%2fwp-content%2fuploads%2f2017%2f08%2fTWDjang-Chaeng-Wattana.jpg&ehk=WHcn32JlAx8ZTvDdAHwMb81EWoU%2fiVLDtNNd6K%2bP6Ao%3d&risl=&pid=ImgRaw&r=0"},
-  {name:'ฮอม บาร์', style:'อาหารกึ่งบาร์', service:'อาหาร เครื่องดื่ม ดนตรี ดีเจ',time:'17:00–24:00', img:"https://www.kinnaii.com/wp-content/uploads/2021/03/hombarnicekinnaii.jpg"},
+  {name:'ฮอม บาร์', style:'อาหารกึ่งบาร์', service:'อาหาร เครื่องดื่ม ดนตรี ดีเจ',time:'17:00-24:00', img:"https://www.kinnaii.com/wp-content/uploads/2021/03/hombarnicekinnaii.jpg"},
 ]);
 const bookinglist = ref([]);
 const tableQuantity = ref(Array(travellist.value.length).fill(0));
